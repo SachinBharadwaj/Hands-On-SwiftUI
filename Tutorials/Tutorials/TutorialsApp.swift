5e27b1b71f5400677f0d836b8c8356f67e66e249
Sachin Bharadwaj <alertsachinbharadwaj@gmail.com>
2021-05-13 00:22:53 +0530

    Tutorials App by Sean Allen

---
+++ Tutorials/Tutorials/TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialsApp.swift
+//  Tutorials
+//
+//  Created by Sachin Bharadwaj on 12/05/21.
+//
+
+import SwiftUI
+
+@main
+struct TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TutorialListView()
+        }
+    }
+}
